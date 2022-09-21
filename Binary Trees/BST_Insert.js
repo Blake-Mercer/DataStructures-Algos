367c5ba6c094b17a5d8ec9fa4d912e09a7dda34d
Blake Mercer <blake.mercer93@gmail.com>
2022-09-21 17:01:21 -0400

    yea

--- Binary Trees/BST_Insert.js
+++ Binary Trees/BST_Insert.js
@@ -1,5 +1,5 @@
 class Node {
-    constructor(value){
+    constructor(value) {
         this.value = value;
         this.left = null;
         this.right = null;
@@ -7,29 +7,29 @@ class Node {
 }
 
 class BinarySearchTree {
-    constructor(){
+    constructor() {
         this.root = null;
     }
-    insert(value){
+    insert(value) {
         var newNode = new Node(value);
-        if(this.root === null){
+        if (this.root === null) {
             this.root = newNode;
             return this;
         }
         var current = this.root;
-        while(true){
-            if(value === current.value) return undefined;
-            if(value < current.value){
-                if(current.left === null){
+        while (true) {
+            if (value === current.value) return undefined;
+            if (value < current.value) {
+                if (current.left === null) {
                     current.left = newNode;
                     return this;
                 }
                 current = current.left;
             } else {
-                if(current.right === null){
+                if (current.right === null) {
                     current.right = newNode;
                     return this;
-                } 
+                }
                 current = current.right;
             }
         }
