7ce99a4caf7052d872a81e0a6247f196b573b0e7
Blake Mercer <blake.mercer93@gmail.com>
2025-01-02 21:58:15 -0500

    testing 2025

---
+++ algorithmpractice/index.mjs
@@ -0,0 +1,34 @@
+import { EventEmitter } from 'events'
+
+export class SubsetSum extends EventEmitter {
+  constructor(sum, set) {
+    super()
+    this.sum = sum
+    this.set = set
+    this.totalSubsets = 0
+  }
+
+  _combine(set, subset) {
+    for (let i = 0; i < set.length; i++) {
+      const newSubset = subset.concat(set[i])
+      this._combine(set.slice(i + 1), newSubset)
+      this._processSubset(newSubset)
+    }
+  }
+
+  _processSubset(subset) {
+    console.log('Subset', ++this.totalSubsets, subset)
+    const res = subset.reduce((prev, item) => (prev + item), 0)
+    if (res === this.sum) {
+      this.emit('match', subset)
+    }
+  }
+
+  start() {
+    this._combine(this.set, [])
+    this.emit('end')
+  }
+}
+
+const run = new SubsetSum(5, [1, 2, 3])
+run.start()
